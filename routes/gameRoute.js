7d2178ab1fe4b3b6841e81e9035cf819103617d5
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-11-14 10:42:14 +0700

    added vrifying character route

--- routes/gameRoute.js
+++ routes/gameRoute.js
@@ -2,10 +2,32 @@ const { Router } = require('express')
 
 const gameRouter = Router()
 
-gameRouter.get('/:consoleName', (req, res) => {
-    const { consoleName } = req.params
-    console.log(consoleName)
-    res.send('connection established')
-})
+// Store character coordinates (example data)
+const levels = {
+    ps2: [
+        { name: 'Yuna', xMin: 219, xMax: 298, yMin: 421, yMax: 462 },
+        { name: 'James Sunderland', xMin: 263, xMax: 310, yMin: 648, yMax: 684 },
+        // Add more characters and coordinates
+    ],
+    ps1: [
+        { name: 'Snake', xMin: 182, xMax: 260, yMin: 400, yMax: 430 },
+        { name: 'Cloud', xMin: 263, xMax: 310, yMin: 648, yMax: 684 },
+        // Add more characters and coordinates
+    ],
+    // Add more levels
+};
+
+// Route to verify a click
+gameRouter.post('/verify', (req, res) => {
+    const { levelId, characterName, x, y } = req.body;
+    //console.log(levelId, characterName, x, y)
+    const character = levels[levelId].find(c => c.name === characterName);
+
+    if (character && x >= character.xMin && x <= character.xMax && y >= character.yMin && y <= character.yMax) {
+        res.json({ found: true, characterName });
+    } else {
+        res.json({ found: false });
+    }
+});
 
 module.exports = gameRouter
